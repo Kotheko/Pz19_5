af43132bfef96e20f84c03ef9199f9f1517bf89b
Ruslan Denisov <necronlive@mail.ru>
2024-01-22 22:26:57 +0900

    Initial

---
+++ Pz19_5.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+class Animal
+{
+public:
+    virtual void Voice()
+    {
+        std::cout << "Животный звук!\n";
+    }
+};
+
+class Cow : public Animal
+{
+public:
+    void Voice() override
+    {
+        std::cout << "Мууууу!\n";
+    }
+};
+
+class Horse : public Animal
+{
+public:
+    void Voice() override
+    {
+        std::cout << "Иигого!\n";
+    }
+};
+
+class Goat : public Animal
+{
+public:
+    void Voice() override
+    {
+        std::cout << "Беееее!\n";
+    }
+};
+
+int main()
+{
+    Animal* animals[4] = { new Animal(), new Cow(), new Horse(), new Goat() };
+
+    for (Animal* a : animals)
+    {
+        a->Voice();
+    }
+}
